24c1f0b1ea8a5a5b5624630680ce04fb479f3952
sina-byn <sina.sina.bayan@protonmail.com>
2022-05-07 16:57:53 +0430

    main: new arrivals section added

---
+++ src/components/NewArrival.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+// Importing Styles
+import styles from '../scss/NewArrival.module.scss';
+
+// Importing Images
+import Img1 from '../assets/img-1.svg';
+import Img2 from '../assets/img-2.svg';
+import Img3 from '../assets/img-3.svg';
+import Img4 from '../assets/img-4.svg';
+
+
+const NewArrival = () => {
+    return (
+        <section className={styles.new_arrival_section}>
+            <h1>NEW ARRIVAL</h1>
+            <ul className={styles.tabs_buttons}>
+                <li>All</li>
+                <li>Apparel</li>
+                <li>Dress</li>
+                <li>Tshirt</li>
+                <li>Bag</li>
+            </ul>
+            <div className={styles.cards_container}>
+                <div className={styles.product_card}>
+                    <img src={Img1} alt="Product One" />
+                    <h6>21WN reversible angora cardigan</h6>
+                    <p>$120</p>
+                </div>
+                <div className={styles.product_card}>
+                    <img src={Img2} alt="Product Two" />
+                    <h6>21WN reversible cardigan</h6>
+                    <p>$120</p>
+                </div>
+                <div className={styles.product_card}>
+                    <img src={Img3} alt="Product Three" />
+                    <h6>21WN reversible cardigan</h6>
+                    <p>$120</p>
+                </div>
+                <div className={styles.product_card}>
+                    <img src={Img4} alt="Product Four" />
+                    <h6>Oblang bag</h6>
+                    <p>$120</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default NewArrival;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 // Importing Components
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import NewArrival from '../components/NewArrival';
 
 const HomePage = () => {
     return (
         <div>
             <Header />
             <Banner />
+            <NewArrival />
         </div>
     );
 };
